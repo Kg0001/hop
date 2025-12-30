e6aa063d5ffca5214a73db733b0da51d6c0432e3
Kaustav Goel <goelkaustav@gmail.com>
2025-12-30 18:14:48 +0530

    chore: set turbopack root

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+	turbopack: {
+		// Set project root to silence multiple lockfile warning
+		root: __dirname,
+	},
 };
 
 export default nextConfig;
